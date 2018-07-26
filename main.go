1a4c45b1b59ae671586c1485fe13564b2a195f9d
Sylvain Rabot <s.rabot@lectra.com>
2018-07-26 13:44:40 +0200

    Fix buffers
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- main.go
+++ main.go
@@ -4,45 +4,45 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "runtime"
+	"fmt"
+	"net"
+	"runtime"
 )
 
 func listen(connection *net.UDPConn, quit chan struct{}) {
-    buffer := make([]byte, 1024)
-    _, remoteAddr, err := 0, new(net.UDPAddr), error(nil)
+	buffer := make([]byte, 1024)
+	_, remoteAddr, err := 0, new(net.UDPAddr), error(nil)
 
-    for err == nil {
-        _, remoteAddr, err = connection.ReadFromUDP(buffer)
-        buffer_tmp := make([]byte, 1024)
+	for err == nil {
+		_, remoteAddr, err = connection.ReadFromUDP(buffer)
+		buffer_tmp := make([]byte, 1024)
 
-        copy(buffer_tmp, buffer)
+		copy(buffer_tmp, buffer)
+		buffer = make([]byte, 1024)
+		go connection.WriteTo(buffer_tmp, remoteAddr)
+	}
 
-        go connection.WriteTo(buffer, remoteAddr)
-    }
-
-    fmt.Println("listener failed - ", err)
-    quit <- struct{}{}
+	fmt.Println("listener failed - ", err)
+	quit <- struct{}{}
 }
 
 func main() {
-    addr := net.UDPAddr{
-        Port: 7778,
-        IP:   net.IP{0, 0, 0, 0},
-    }
+	addr := net.UDPAddr{
+		Port: 7778,
+		IP:   net.IP{0, 0, 0, 0},
+	}
 
-    connection, err := net.ListenUDP("udp", &addr)
+	connection, err := net.ListenUDP("udp", &addr)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    quit := make(chan struct{})
+	quit := make(chan struct{})
 
-    for i := 0; i < runtime.NumCPU(); i++ {
-        go listen(connection, quit)
-    }
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go listen(connection, quit)
+	}
 
-    <-quit // hang until an error
+	<-quit // hang until an error
 }
